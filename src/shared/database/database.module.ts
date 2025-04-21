18de6b04742c85af772a3a95a582ec89026951ae
danillotorresdev <danilloept@gmail.com>
2025-04-21 12:30:49 -0300

    feat: add bank accounts module with controller, service, and repository; implement CRUD operations and ownership validation

--- src/shared/database/database.module.ts
+++ src/shared/database/database.module.ts
@@ -1,11 +1,17 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from 'src/shared/database/prisma.service';
+import { BankAccountsRepository } from 'src/shared/database/repositories/bank-accounts.repositories';
 import { CategoriesRepository } from 'src/shared/database/repositories/categories.repositories';
 import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
 
 @Global()
 @Module({
-  providers: [PrismaService, UsersRepository, CategoriesRepository],
-  exports: [UsersRepository, CategoriesRepository],
+  providers: [
+    PrismaService,
+    UsersRepository,
+    CategoriesRepository,
+    BankAccountsRepository,
+  ],
+  exports: [UsersRepository, CategoriesRepository, BankAccountsRepository],
 })
 export class DatabaseModule {}
